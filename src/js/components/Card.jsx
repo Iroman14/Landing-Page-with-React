87f5214bd8b8e021110fdb478e77f1b9f01d3a72
Iroman14 <134985621+Iroman14@users.noreply.github.com>
2025-02-17 10:16:46 +0000

    Mejora del proyecto

--- src/js/components/Card.jsx
+++ src/js/components/Card.jsx
@@ -1,61 +1,16 @@
-function Card (props) {
-    const { image1, title1, description1, image2, title2, description2, image3, title3, description3, image4, title4, description4 } = props.cards;
+function Card(props) {
+    const { image, title, description } = props.cards;
 
     return (
-        <div className="container">
-            <div className="row">
-                <div className="col-12 col-sm-12 col-md-6 col-lg-3 mx-auto mb-5">
-                    <div className="card">
-                        <img src={image1} className="card-img-top" alt={title1} />
-                        <hr />
-                        <div className="card-body">
-                            <h5 className="card-title">{title1}</h5>
-                            <p className="card-text">{description1}</p>
-                        </div>
-                        <div className="card-footer">
-                            <a href="#" className="btn btn-primary">Ver más</a>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-12 col-sm-12 col-md-6 col-lg-3 mx-auto mb-5">
-                    <div className="card">
-                        <img src={image2} className="card-img-top" alt={title2} />
-                        <hr />
-                        <div className="card-body">
-                            <h5 className="card-title">{title2}</h5>
-                            <p className="card-text">{description2}</p>
-                        </div>
-                        <div className="card-footer">
-                            <a href="#" className="btn btn-primary">Ver más</a>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-12 col-sm-12 col-md-6 col-lg-3 mx-auto mb-5">
-                    <div className="card">
-                        <img src={image3} className="card-img-top" alt={title3} />
-                        <hr />
-                        <div className="card-body">
-                            <h5 className="card-title">{title3}</h5>
-                            <p className="card-text">{description3}</p>
-                        </div>
-                        <div className="card-footer">
-                            <a href="#" className="btn btn-primary">Ver más</a>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-12 col-sm-12 col-md-6 col-lg-3 mx-auto mb-5">
-                    <div className="card">
-                        <img src={image4} className="card-img-top" alt={title4} />
-                        <hr />
-                        <div className="card-body">
-                            <h5 className="card-title">{title4}</h5>
-                            <p className="card-text">{description4}</p>
-                        </div>
-                        <div className="card-footer">
-                            <a href="#" className="btn btn-primary">Ver más</a>
-                        </div>
-                    </div>
-                </div>
+        <div className="card">
+            <img src={image} className="card-img-top" alt={title} />
+            <hr />
+            <div className="card-body">
+                <h5 className="card-title">{title}</h5>
+                <p className="card-text">{description}</p>
+            </div>
+            <div className="card-footer">
+                <a href="#" className="btn btn-primary">Ver más</a>
             </div>
         </div>
     );
--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -7,27 +7,42 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 const cards = [
 	{
-		image1: "https://i0.wp.com/junilearning.com/wp-content/uploads/2020/06/python-programming-language.webp?fit=800%2C800&ssl=1",
-		title1: "Python",
-		description1: "Un lenguaje de programación interpretado y de alto nivel, conocido por su sintaxis sencilla y legible. Es ideal para desarrollo web, ciencia de datos, inteligencia artificial y automatización.",
-		image2: "https://conclase.net/imagen/c/curso/C++logo.svg",
-		title2: "C++",
-		description2: "Un lenguaje de programación de propósito general y bajo nivel, ampliamente utilizado en sistemas operativos, software embebido y aplicaciones que requieren alto rendimiento y control sobre la memoria.",
-		image3: "https://okhosting.com/wp-content/uploads/2019/01/java.jpg",
-		title3: "Java",
-		description3: "Un lenguaje de programación orientado a objetos que es popular en el desarrollo de aplicaciones empresariales, móviles (Android) y sistemas escalables.",
-		image4: "https://contentstatic.techgig.com/photo/117361179/why-every-c-developer-should-give-rust-programming-language-a-try.jpg?26732",
-		title4: "Rust",
-		description4: "Un lenguaje moderno enfocado en seguridad y rendimiento, diseñado para evitar errores de memoria y facilitar la programación concurrente. Es una alternativa eficiente a C y C++ en el desarrollo de sistemas.",
+		image: "https://i0.wp.com/junilearning.com/wp-content/uploads/2020/06/python-programming-language.webp?fit=800%2C800&ssl=1",
+		title: "Python",
+		description: "Un lenguaje de programación interpretado y de alto nivel, conocido por su sintaxis sencilla y legible. Es ideal para desarrollo web, ciencia de datos, inteligencia artificial y automatización.",
 	},
+	{
+		image: "https://conclase.net/imagen/c/curso/C++logo.svg",
+		title: "C++",
+		description: "Un lenguaje de programación de propósito general y bajo nivel, ampliamente utilizado en sistemas operativos, software embebido y aplicaciones que requieren alto rendimiento y control sobre la memoria.",
+	},
+	{
+		image: "https://okhosting.com/wp-content/uploads/2019/01/java.jpg",
+		title: "Java",
+		description: "Un lenguaje de programación orientado a objetos que es popular en el desarrollo de aplicaciones empresariales, móviles (Android) y sistemas escalables.",
+	},
+	{
+		image: "https://contentstatic.techgig.com/photo/117361179/why-every-c-developer-should-give-rust-programming-language-a-try.jpg?26732",
+		title: "Rust",
+		description: "Un lenguaje moderno enfocado en seguridad y rendimiento, diseñado para evitar errores de memoria y facilitar la programación concurrente. Es una alternativa eficiente a C y C++ en el desarrollo de sistemas.",
+
+	}
 ];
 
-function Home () {
+function Home() {
 	return (
 		<div className="text-center">
 			<Navbar />
 			<Jumbotron />
-			<Card cards={cards[0]} />
+			<div className="container">
+				<div className="row">
+					{cards.map((card, index) => (
+						<div className="col-12 col-sm-12 col-md-6 col-lg-3 mx-auto mb-5">
+							<Card key={index} cards={card} />
+						</div>
+					))};
+				</div>
+			</div>
 			<Footer />
 		</div>
 	);
